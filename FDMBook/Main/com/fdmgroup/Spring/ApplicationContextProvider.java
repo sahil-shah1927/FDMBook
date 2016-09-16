f1120ba923dec131919a3e8eab79bbe50e138a7f
sahil.shah <sahil.shah@TRNNYC30429.fdmgroup.local>
2016-09-16 17:04:48 -0400

    Added Kishan's ApplicationContextProvider

---
+++ FDMBook/Main/com/fdmgroup/Spring/ApplicationContextProvider.java
@@ -0,0 +1,37 @@
+package com.fdmgroup.Spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * ApplicationContextProvider gives static access to the Application
+ * Context which is created with beans.xml by default. 
+ *
+ *	@author Kishan Patel
+ */
+public class ApplicationContextProvider implements ApplicationContextAware
+{
+
+	private static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	
+	public ApplicationContextProvider()
+	{
+
+	}
+	 
+    public static ApplicationContext getApplicationContext() 
+    {
+        return context;
+    }
+    
+	@Override
+	public void setApplicationContext(ApplicationContext ctx) throws BeansException 
+	{
+		context = ctx;
+		
+	}
+
+}
