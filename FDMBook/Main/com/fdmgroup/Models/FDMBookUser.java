3e8448b5614e4d299a45cb8494a3c5061a333f67
sahil.shah <sahil.shah@TRNNYC30429.fdmgroup.local>
2016-09-16 16:59:15 -0400

    Added a "beans.xml" - Not done just yet.

--- FDMBook/Main/com/fdmgroup/Models/FDMBookUser.java
+++ FDMBook/Main/com/fdmgroup/Models/FDMBookUser.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 
@@ -17,10 +21,12 @@ import javax.persistence.Entity;
  *
  */
 @Entity
+@Table(name = "FDMBOOKUSERS")
 public class FDMBookUser 
 {
 	
-	@Column
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
 	
 	@Column
